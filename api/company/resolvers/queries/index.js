d5594a20818f514fe1dc8378102248494dffc512
Paula Pompeo <paulapompeoc@gmail.com>
2020-12-31 11:45:19 +0100

    fix bug

--- api/company/resolvers/queries/index.js
+++ api/company/resolvers/queries/index.js
@@ -1,9 +1,7 @@
 const companies = require("./companies");
 const company = require("./company");
-const employees = require("./employees");
 
 module.exports = {
   companies,
   company,
-  employees
 }
